797d279d775290605f3aad397ab337c464ff4f8d
Dahalkaran <dahalkaran27@gmai.com>
2025-01-03 20:03:43 +0530

    just routes adjust

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
   const password = document.getElementById('password').value;
 
   try {
-    const response = await axios.post('http://localhost:3000/users/login', {
+    const response = await axios.post('/users/login', {
       email,
       password,
     });
